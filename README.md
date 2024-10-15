0e83e82ca31ba38325fa361b087a918f8a5e402e
Tepz <tayyab2607@gmail.com>
2024-10-15 04:41:36 +0200

    Update

---
+++ README.md
@@ -0,0 +1,72 @@
+# Manga Viewer Web Application
+
+Welcome to the Manga Viewer web application! This project represents my first deep dive into web application development, and I have gained invaluable insights and skills throughout this journey.
+
+## Overview
+
+The Manga Viewer is a simple yet functional web application that allows users to browse and read manga offline. It features a user-friendly interface where you can navigate through various chapters and read your favorite manga. Additionally, it includes a bookmarking feature that remembers your last read chapter, so you never have to worry about losing your place.
+
+### Features
+
+- **Chapter Navigation**: Easily move between previous and next chapters.
+- **Image Display**: View manga pages in a clean layout, optimized for all screen sizes.
+- **Dynamic Content**: The application reads from a local directory structure to serve manga chapters and images.
+
+## Technology Stack
+
+This project is built using:
+
+- **Go**: The backend is powered by Go, utilizing its `net/http` package to handle HTTP requests and serve content.
+- **HTML/CSS**: The front-end interface is designed with HTML and CSS for structure and styling.
+
+## Installation
+
+### Windows
+Visit the Releases section of this repository to download the latest compiled binary for Windows.
+Extract the Files
+
+Once downloaded, extract the contents of the ZIP file to a folder of your choice.
+
+### Linux
+   ```bash
+   git clone https://github.com/haya123421321/Manga-Reader.git
+   cd Manga-Reader
+   go build -o Manga-Reader main.go
+   ```
+
+## Post
+Place your manga files in the `mangas` directory, like so:
+```
+├── mangas
+│   └── Berserk
+│       └── Berserk 001
+│           ├── 0.jpg
+│           ├── 1.jpg
+│           ├── 2.jpg
+│           ├── 3.jpg
+│           ├── 4.jpg
+│           ├── ...
+└── README.md
+
+```
+
+
+Start the program and  navigate to `http://127.0.0.1:8080` in your browser to access the application.
+
+## Future Improvements
+
+As this is my first web application, I'm still learning and planning to implement new features, including:
+
+- Improved error handling and user feedback.
+- Enhanced styling for a better user experience.
+- Add support for zip and cbz files
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Feel free to contribute to this project! Open an issue or submit a pull request if you have suggestions or improvements.
+
+And lasty thank you to ChatGPT for writing this README, couldn't have done it without you.
---
+++ main.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type manga_page_data struct {
+	Images []string
+	Name string
+	Next string
+	Previous string
+}
+
+type manga_information struct {
+	Title string `json:title`
+	Data reading_data `json:data`
+}
+
+type reading_data struct {
+	Last_read_chapter string `json:last_read_chapter`
+}
+
+var (
+	Titles []string
+	handlers_made []string
+) 
+
+
+func main() {
+	http.Handle("/mangas/", http.StripPrefix("/mangas/", http.FileServer(http.Dir("./mangas"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+
+	http.HandleFunc("/", home)
+
+	directoryss,err := os.ReadDir("mangas")
+	if err != nil {
+		fmt.Println("couldnt read mangas directory")
+	}
+
+	for _,directory := range directoryss {
+		if directory.IsDir() {
+			Titles = append(Titles, directory.Name())
+			url_encode_title := url.PathEscape(directory.Name())
+			http.HandleFunc("/" + url_encode_title + "/", func(w http.ResponseWriter, r *http.Request) {
+				manga_page(w, r, directory.Name())
+			})
+		}
+	}
+	
+	go func() {
+		err = http.ListenAndServe("127.0.0.1:8080", nil)
+		if err != nil {
+			fmt.Println("Failed to open port 8080 on localhost")
+			return
+		}
+	}()
+
+	fmt.Println("Listening on port 8080")
+	fmt.Println("Go to http://127.0.0.1:8080 to access the viewer.")
+	for {}
+}
+
+type home_data struct {
+	Titles []string
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	tmpl,err := template.ParseFiles("home.html")
+	if err != nil {
+		fmt.Println("Coudlnt parse home.html")
+		fmt.Println(err)
+		return
+	}
+
+	d := home_data {
+		Titles: Titles,
+	}
+
+	tmpl.Execute(w, d)
+	
+}
+
+func manga_page(w http.ResponseWriter, r *http.Request, title string) {
+	file,err := os.ReadFile("data.json")
+	if err != nil {
+		os.Create("data.json")
+	}
+	
+	var json_data []manga_information
+	json.Unmarshal(file, &json_data)
+	
+	var title_in_data bool
+	for _,j := range json_data {
+		if j.Title == title {
+			title_in_data = true
+			break
+		}
+	}
+	
+	manga_path := "/mangas/" + title
+	files,err := os.ReadDir("." + manga_path)
+	if err != nil {
+		fmt.Println("Couldnt read from manga directory " + manga_path)
+	}
+
+
+	var chapters []string
+	for _,file := range files {
+		if file.IsDir() {
+			chapters = append(chapters, file.Name())
+		}
+	}
+	
+	var manga_index_in_json int
+	var chapter_index = 0
+	if !title_in_data {
+		data := reading_data{Last_read_chapter: chapters[0]}
+		json_data = append(json_data, manga_information{Title: title, Data: data})
+		new_json_data,_ := json.MarshalIndent(json_data, "", "\t")
+		os.WriteFile("data.json", new_json_data, 0600)
+		
+		chapter_index = 0
+	} else {
+		manga_index_in_json = findMangaIndexFromJson(json_data, title)
+		for i,chapter := range chapters {
+			if json_data[manga_index_in_json].Data.Last_read_chapter == chapter {
+				chapter_index = i
+				break
+			}
+		}
+	}
+
+
+	chapter := chapters[chapter_index]
+
+	
+	for _,c := range chapters {
+		safeHandler("/" + url.PathEscape(title) + "/" + url.PathEscape(c), func(w http.ResponseWriter, r *http.Request) {
+			load_images(w, title, chapters, manga_path, c)
+		})
+	}
+
+
+	http.Redirect(w, r, "/" + title + "/" + chapter, http.StatusSeeOther)
+}
+
+func load_images(w http.ResponseWriter, title string, chapters []string, manga_path string, chapter string) {
+	chapter_path := manga_path + "/" + chapter
+	image_files,err := os.ReadDir("." + chapter_path)
+	if err != nil {
+		return
+	}
+
+	var files_string []string
+	for _,file := range image_files {
+		files_string = append(files_string, chapter_path + "/" + file.Name())
+	}
+	var chapter_index int
+	for i,j := range chapters {
+		if j == chapter {
+			chapter_index = i
+			break
+		}
+	} 
+	
+	var next string
+	var previous string
+	if chapter_index != 0 {
+		previous = chapters[chapter_index - 1]
+	} else {
+		previous = chapters[0]
+	}
+
+	if chapter_index != len(chapters) - 1 {
+		next = chapters[chapter_index + 1]
+	} else {
+		next = chapters[chapter_index]
+	}
+
+	d := manga_page_data{
+		Images: files_string,
+		Name: chapter,
+		Next: "/" + title + "/" + next,
+		Previous: "/" + title + "/" + previous,
+	}
+
+
+	tmpl,err := template.ParseFiles("manga_page.html")
+	if err != nil {
+		fmt.Println("Couldnt make template that parses index.html")
+		fmt.Println(err)
+		return
+	}
+
+	tmpl.Execute(w, d)
+
+
+	file,err := os.ReadFile("data.json")
+	if err != nil {
+		os.Create("data.json")
+	}
+	
+	var json_data []manga_information
+	json.Unmarshal(file, &json_data)
+
+	manga_index_in_json := findMangaIndexFromJson(json_data, title)
+	
+	if json_data[manga_index_in_json].Data.Last_read_chapter != chapter  {
+		json_data[manga_index_in_json].Data.Last_read_chapter = chapter
+		new_json_data,_ := json.MarshalIndent(json_data, "", "\t")
+		os.WriteFile("data.json", new_json_data, 0600)
+	}
+}
+
+func safeHandler(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	var handler_already_made bool
+	for _,handle := range handlers_made {
+		if handle == pattern {
+			handler_already_made = true
+			break
+		}
+	}
+	if !handler_already_made {
+		http.HandleFunc(pattern, handler)
+		handlers_made = append(handlers_made, pattern)
+	}
+}
+
+func findMangaIndexFromJson(json_data []manga_information, title string) int {
+	for i, j := range json_data {
+		if j.Title == title {
+			return i
+		}
+	}
+	return -1
+}
