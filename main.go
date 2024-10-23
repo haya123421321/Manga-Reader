2bd043e8f1fbf0e26ab03e13bfb8470e1e757d87
Tepz <tayyab2607@gmail.com>
2024-10-23 16:10:45 +0200

    Update

--- main.go
+++ main.go
@@ -178,32 +178,59 @@ func load_images(w http.ResponseWriter, title string, chapters []string, manga_p
 	// Custom sorting through image files
 	if len(image_files) > 9 {
 		var Not_Numeric_Format_Checker int
-		for i:=0;i<len(image_files);i++ {
-			// check three times if its a numeric format, and if not break
+		var Is_Numeric_Format_Checker int
+
+		for _,file := range image_files {
 			if Not_Numeric_Format_Checker == 3 {
 				fmt.Println("Not using numeric formatting, using default sort")
+				files_string = []string{}
+				for _,file := range image_files {
+					files_string = append(files_string, chapter_path + "/" + file.Name())
+				}
+				break
+			} else if Is_Numeric_Format_Checker == 3 {
+				files_string = []string{}
+				for i:=0;i<len(image_files);i++{
+					for _,file := range image_files {
+						if strings.Split(file.Name(), ".")[0] == strconv.Itoa(i) {
+							files_string = append(files_string, chapter_path + "/" + file.Name())
+							break
+						} 
+					}
+				}
 				break
 			}
 
 			var found_number bool
-			for _,file := range image_files {
+			for i:=0;i<len(image_files);i++{
 				if strings.Split(file.Name(), ".")[0] == strconv.Itoa(i) {
-					files_string = append(files_string, chapter_path + "/" + file.Name())
 					found_number = true
 					break
 				} 
 			}
 			if !found_number {
 				Not_Numeric_Format_Checker += 1
+			} else if found_number {
+				Is_Numeric_Format_Checker += 1
 			}
 		}
 		
 		if len(image_files) != len(files_string) {
-			files_string = []string{}
-			for _,file := range image_files {
-				files_string = append(files_string, chapter_path + "/" + file.Name())
+			fmt.Println("WARNING: Images are not in order")
+			mp := make(map[string]struct{})
+			for _,file := range files_string {
+				mp[file] = struct{}{}
 			}
-		}
+
+			for _,file := range image_files {
+				_,exists := mp[chapter_path + "/" + file.Name()]
+				if exists == false {
+					files_string = append(files_string, chapter_path + "/" + file.Name())
+					}
+				}
+			mp = nil
+			}
+		image_files = nil
 	} else {
 		for _,file := range image_files {
 			files_string = append(files_string, chapter_path + "/" + file.Name())
